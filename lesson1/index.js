689aa3f0897de4bc72dd03b568693d1106820abe
Yulia <yulia0903e@gmail.com>
2023-10-03 20:56:21 +0300

    add hw1

---
+++ lesson1/index.js
@@ -0,0 +1,15 @@
+alert("Hello!!!");
+
+let name = prompt("What is your name?");
+
+// prompt("What is your name?");
+
+let from = prompt("Where are your from?");
+
+let meeting = "Nice to meet you,";
+
+let country = "You are from";
+
+let result = meeting + " " + name + "!" + " " + country + " " + from + "!";
+
+alert(result);
