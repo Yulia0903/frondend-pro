d01ae781fba648df069308eabf01193675383e0e
Yulia <yulia0903e@gmail.com>
2023-12-09 20:19:03 +0200

    add hw21

--- hw21/index.js
+++ hw21/index.js
@@ -1,21 +1,20 @@
-
 const table = document.createElement("table");
 document.body.appendChild(table);
 
+const rows = 10;
+const cols = 10;
 
 let counter = 1;
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < rows; i++) {
   const row = table.insertRow();
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < cols; j++) {
     const cell = row.insertCell();
     cell.textContent = counter++;
   }
 }
 
-
 table.style.borderCollapse = "collapse";
 table.style.width = "100%";
-
 
 const cells = document.querySelectorAll("td, th");
 cells.forEach((cell) => {
