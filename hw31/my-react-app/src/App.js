e812429b2a693c8080aceb8e001c1457a1779287
Yulia <yulia0903e@gmail.com>
2024-01-19 19:15:59 +0200

    add hw31

---
+++ hw31/my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Это мой перый React проект</h1>
+        <p>Привет! Ты на начальной старинце моего React-проекта.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
